aa96ec24a05deb647f44cb850ec73cd542e5dbaa
Alberto19 <junioralberto19@gmail.com>
2016-05-30 13:35:26 -0300

    Projeto Biblioteca Com Vraptor- 4
    Sistema de uma Biblioteca

---
+++ src/main/java/br/com/impacta/Biblioteca/dao/UsuarioDAO.java
@@ -0,0 +1,45 @@
+package br.com.impacta.Biblioteca.dao;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import br.com.impacta.Biblioteca.model.Usuario;
+
+
+@RequestScoped
+public class UsuarioDAO {
+
+private final EntityManager em;
+	
+	@Inject
+	public UsuarioDAO(EntityManager em) {
+		this.em = em;
+	}
+	
+	@Deprecated
+	public UsuarioDAO() {
+		this(null); // para uso do CDI
+	}
+	//Metodo para Salvar Usuario
+	public void Salvar(Usuario usuario) {
+		em.persist(usuario);
+	}	
+	//Metodo para Remover Usuario
+	public void Remover(Usuario usuario) {
+		em.remove(Buscar(usuario));
+	}
+	//Metodo para Buscar Usuario
+	public Usuario Buscar(Usuario usuario) {
+		return em.find(Usuario.class, usuario.getId());
+	}
+	//Metodo Para Verificar se um Usuario Existe
+	public boolean existe(Usuario usuario) {
+		return !em.createQuery("select u from Usuario u where u.login = "
+			+ ":login and u.senha = :senha", Usuario.class)
+			.setParameter("nome", usuario.getLogin())
+			.setParameter("senha", usuario.getSenha())
+			.getResultList().isEmpty();
+	}
+	
+	
+}
---
+++ src/main/java/br/com/impacta/Biblioteca/util/JPAUtil.java
@@ -0,0 +1,13 @@
+package br.com.impacta.Biblioteca.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	public static EntityManager criaEntityManager() {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("default");
+		return factory.createEntityManager();
+	}
+}
